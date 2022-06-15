2b62fc68468942cbe53f565df162232a55f566b3
Kay <kard@hotmail.co.uk>
2022-06-15 11:17:43 +0100

    changed feature test name to viewing homepage

--- Gemfile
+++ Gemfile
@@ -5,9 +5,13 @@ source "https://rubygems.org"
 # gem "rails"
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'pg'
 
 group :test do
   gem 'rspec'
   gem 'capybara'
-  gem 'pg'
+end
+
+group :development, :test do
+  gem 'rubocop', '1.20'
 end
--- spec/features/view_spaces_spec.rb
+++ spec/features/view_spaces_spec.rb
@@ -9,4 +9,6 @@ feature "Viewing spaces" do
     expect(page).to have_content "Name: Buckingham Palace, Description: The biggest house in the world, Price: mil$/night"
     expect(page).to have_content "Name: Happy House, Description: A really cosy space, where you can relax, Price: 24$/night"
   end
+
+  # need a test that begins from the homepage
 end
