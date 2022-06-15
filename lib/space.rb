0bb8ac11e3e330134cea03a9a1630666f0674f1a
Kay <kard@hotmail.co.uk>
2022-06-15 20:37:32 +0100

    three space class tests now pass

--- lib/space.rb
+++ lib/space.rb
@@ -11,21 +11,9 @@ class Space
   end
 
   def self.all
-    spaces = [{
-      name: 'Happy House',
-      description: 'A really cosy space, where you can relax', 
-      price: '24$/night'},
-      {
-        name: 'Buckingham Palace',
-        description: 'The biggest house in the world',
-        price: 'mil$/night'
-      }
-    ]
-    spaces.map { |space|
-      Space.new(name: space[:name],
-        description: space[:description], 
-        price: space[:price])
-    }
+    Space.choose_database
+    table = @connection.exec ("SELECT * FROM spaces;")
+    table.map {|row| row.values_at('name', 'description')}
   end
 
   def self.add(space_name, description)
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Space do
 
   it 'adds a different space with a different name and description' do
     Space.add('The Sea', "It's very wet!")
-    expect(Space.show_most_recent_space).to include('The Sea', "It's very wet!")
+    expect(Space.show_most_recent_space).to eq ['The Sea', "It's very wet!"]
   end
 
-  xit 'returns the multiple names when they have been added' do
-    Space.add('The Sea')
-    Space.add('The Moon')
-    expect(Space.show_most_recent_space).to eq 'The Moon'
-    expect(Space.names).to eq ['The Sea', 'The Moon']
+  it 'returns the multiple names and descriptions when they have been added' do
+    Space.add('The Sea', "It's very wet!")
+    Space.add('The Moon', 'The Rockiest Place you could imagine...')
+    expect(Space.show_most_recent_space).to eq ['The Moon', 'The Rockiest Place you could imagine...']
+    expect(Space.all).to eq [['The Sea', "It's very wet!"], ['The Moon', 'The Rockiest Place you could imagine...']]
   end
 end
