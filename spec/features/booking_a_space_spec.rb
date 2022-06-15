9d4945e24fc05d142646ad6ac5031cbde738c33b
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-15 15:20:32 +0100

    Update feature test to only click on the first requests button

--- spec/features/booking_a_space_spec.rb
+++ spec/features/booking_a_space_spec.rb
@@ -1,8 +1,9 @@
 feature "Booking a space" do 
   scenario "when a user wants to book a space" do 
     visit('/makersbnb/spaces')
-    click_button('requests')
-    expect(page).to have_content "Your booking is now confirmed"
+    #test clicking the first requests button and not all the requests buttons 
+    click_button('requests', match: :first) 
+    expect(page).to have_content "Your booking request is now confirmed"
   end 
 
   # needs more tests to cover the full functionality of making a booking request
