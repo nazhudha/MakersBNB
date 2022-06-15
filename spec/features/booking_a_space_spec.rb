7b416b982f6d42a52f6472eceeb2a4be4c56f103
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-15 16:55:14 +0100

    Add feature tests for approve message

--- spec/features/booking_a_space_spec.rb
+++ spec/features/booking_a_space_spec.rb
@@ -1,10 +1,17 @@
 feature "Booking a space" do 
-  scenario "when a user wants to book a space" do 
+  scenario "User can book a space and receives a confirmation" do 
     visit('/makersbnb/spaces')
-    #test clicking the first requests button and not all the requests buttons 
+    # test clicking the first requests button and not all the requests buttons 
     click_button('requests', match: :first) 
     expect(page).to have_content "Your booking request is now confirmed"
   end 
 
+  context "After a user has booked a space" do 
+    scenario "User recieves an automatic approval message" do 
+    visit('/makersbnb/approved')
+    click_button('Ok')
+    expect(page).to have_content "Your booking has now been approved"
+  end 
+end 
   # needs more tests to cover the full functionality of making a booking request
 end 
