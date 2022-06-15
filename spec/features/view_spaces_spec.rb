49ae48c1ba715cbd42825bd6464e3e26d1a1f4fb
Kay <kard@hotmail.co.uk>
2022-06-15 20:22:35 +0100

    added tests for the show most recent space and names method when there are multiple spaces added

--- spec/features/view_spaces_spec.rb
+++ spec/features/view_spaces_spec.rb
@@ -10,8 +10,6 @@ feature "Viewing spaces" do
     expect(page).to have_content "Name: Happy House, Description: A really cosy space, where you can relax, Price: 24$/night"
   end
 
-  
-
   # need a test that begins from the homepage
   # need a separate test for the menu
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe Space do
     Space.add('The Sea')
     expect(Space.show_most_recent_space).to include('The Sea')
   end
+
+  it 'returns the multiple names when they have been added' do
+    Space.add('The Sea')
+    Space.add('The Moon')
+    expect(Space.show_most_recent_space).to eq 'The Moon'
+    expect(Space.names).to eq ['The Sea', 'The Moon']
+  end
 end
