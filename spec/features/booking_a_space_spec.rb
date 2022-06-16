8f3ca6653ecd34a0896e8e3586fcc4310ad4fda1
Kay <kard@hotmail.co.uk>
2022-06-16 10:03:38 +0100

    fixed booking space test

--- spec/features/booking_a_space_spec.rb
+++ spec/features/booking_a_space_spec.rb
@@ -1,5 +1,8 @@
 feature "Booking a space" do 
   scenario "when a user wants to book a space" do 
+    visit '/makersbnb/add'
+    fill_in 'Name', with: 'Buckingham Palace'
+    click_on 'Submit'
     visit('/makersbnb/spaces')
     click_button('requests')
     expect(page).to have_content "Your booking request is now confirmed"
