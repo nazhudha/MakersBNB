7d55d52e38df49d83e327a0f5bc015a3475af810
Kay <kard@hotmail.co.uk>
2022-06-16 10:00:05 +0100

    added coverage checker simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,15 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 require './spec/test_helpers.rb'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 Capybara.app = MakersBNB
 
