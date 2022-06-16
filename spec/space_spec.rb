518a8dfad5580668984b24476bb53f45bc6b274a
Kay <kard@hotmail.co.uk>
2022-06-16 18:34:27 +0100

    added ability to add price

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,31 +1,31 @@
 require 'space'
 
 RSpec.describe Space do
-  context ".all for name and description" do
+  context ".all" do
     it "Returns all spaces" do
-      Space.add("Happy House", "A really cosy space, where you can relax")
-      Space.add("Buckingham Palace", "The biggest house in the world")
-      expect(Space.all).to eq [["Happy House", "A really cosy space, where you can relax", nil], ["Buckingham Palace", "The biggest house in the world", nil]]
+      Space.add("Happy House", "A really cosy space, where you can relax", "200")
+      Space.add("Buckingham Palace", "The biggest house in the world", "4000")
+      expect(Space.all).to eq [["Happy House", "A really cosy space, where you can relax", "£200.00"], ["Buckingham Palace", "The biggest house in the world", "£4,000.00"]]
     end
     # this test needs to be modified to reflect the all method [DONE]
     # add method needs to be able to add a price [PENDING]
     # once the add method is complete, we can use the add method to give Space.all its eq value
 
-    it 'adds a space with a name and description' do
-      Space.add('The Moon', 'The Rockiest Place you could imagine...')
-      expect(Space.show_most_recent_space).to eq ['The Moon', 'The Rockiest Place you could imagine...']
+    it 'adds a space with a name, description and price' do
+      Space.add('The Moon', 'The Rockiest Place you could imagine...', '50.50')
+      expect(Space.show_most_recent_space).to eq ['The Moon', 'The Rockiest Place you could imagine...', '£50.50']
     end
 
     it 'adds a different space with a different name and description' do
-      Space.add('The Sea', "It's very wet!")
-      expect(Space.show_most_recent_space).to eq ['The Sea', "It's very wet!"]
+      Space.add('The Sea', "It's very wet!", '400.60')
+      expect(Space.show_most_recent_space).to eq ['The Sea', "It's very wet!", "£400.60"]
     end
 
     it 'returns the multiple names and descriptions when they have been added' do
-      Space.add('The Sea', "It's very wet!")
-      Space.add('The Moon', 'The Rockiest Place you could imagine...')
-      expect(Space.show_most_recent_space).to eq ['The Moon', 'The Rockiest Place you could imagine...']
-      expect(Space.all).to eq [['The Sea', "It's very wet!", nil], ['The Moon', 'The Rockiest Place you could imagine...', nil]]
+      Space.add('The Sea', "It's very wet!", '400.60')
+      Space.add('The Moon', 'The Rockiest Place you could imagine...', '50.50')
+      expect(Space.show_most_recent_space).to eq ['The Moon', 'The Rockiest Place you could imagine...', '£50.50']
+      expect(Space.all).to eq [['The Sea', "It's very wet!", "£400.60"], ['The Moon', 'The Rockiest Place you could imagine...', '£50.50']]
     end
   end
 end
