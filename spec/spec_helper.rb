a513a357376d6889e90d6577caabaf120b701fac
Kay <kard@hotmail.co.uk>
2022-06-16 19:13:15 +0100

    removed SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,12 @@ require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   # Want a nice code coverage website? Uncomment this next line!
+#   # SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
 
 Capybara.app = MakersBNB
 
