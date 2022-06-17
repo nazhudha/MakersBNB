033e8f9856188a6ec798bb4610b374f54b067a88
Kay <kard@hotmail.co.uk>
2022-06-17 12:36:55 +0100

    .create and .details methods pass using an instance variable

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,10 @@
 class User
 
   def self.create(name, user_name, email, password)
+    @details = [name, user_name, email]
   end
 
   def self.details
-    ['Gerry', 'gerry2', 'gerry@theger.co']
+    @details
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe User do
     User.create('Gerry', 'gerry2', 'gerry@theger.co', 'sugar')
     expect(User.details).to eq ['Gerry', 'gerry2', 'gerry@theger.co']
   end
+
+  it 'takes different user details and returns the different name' do
+    User.create('Sandy', 'sansan', 'sandy@theger.co', 'sugarpie')
+    expect(User.details).to eq ['Sandy', 'sansan', 'sandy@theger.co']
+  end
 end
