6881daceb8ef5e1f0989cf19d529ed5d20ce4d93
Amir Khan <ak2022dev@gmail.com>
2022-06-17 12:54:26 +0100

    Test-driven dummy login page now has input fields for username and password and a submit button. Passes test

--- spec/features/view_login_spec.rb
+++ spec/features/view_login_spec.rb
@@ -1,8 +1,18 @@
 feature "login page" do
-  context "basic connection server/page connection test" do
-    it "Returns a basic text message initially to confirm server connection" do
+#  TDD makes below obsolete
+#  context "basic connection server/page connection test" do
+#    it "Returns a basic text message initially to confirm server connection" do
+#      visit('/makersbnb/login')
+#      expect(page).to have_content "Login page"
+#    end
+#  end
+  context "basic form test" do
+    it "Displays title message, username and password input fields and button to submit" do
       visit('/makersbnb/login')
       expect(page).to have_content "Login page"
+      fill_in 'username', with: 'myusername'
+      fill_in 'password', with: 'mypassword'
+      click_button('Submit')
     end
   end
 end
